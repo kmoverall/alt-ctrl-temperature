92fdc65ece1e5e630469b0bcd23d639388d979e7
kmoverall <kmoverall@gmail.com>
2016-10-02 23:28:58 -0400

    Added a slider that responds to the thermometer reading

---
+++ Assets/UpdateThermometer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Slider))]
+public class UpdateThermometer : MonoBehaviour {
+    public float minTemp;
+    public float maxTemp;
+
+    Slider slider;
+
+	void Start () {
+        slider = GetComponent<Slider>();
+	}
+	
+	void Update () {
+        slider.value = (Thermometer.temperature - minTemp) / (maxTemp - minTemp);
+	}
+
+    void OnValidate() {
+        if (maxTemp <= minTemp) {
+            maxTemp = minTemp + 0.01f;
+        }
+    }
+}

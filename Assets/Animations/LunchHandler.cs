a7c9abc1b9d6b09234c0003ecb5fbe29d05cf188
kmoverall <kmoverall@gmail.com>
2016-11-01 23:07:46 -0400

    Fully implemented lunch prep animation

---
+++ Assets/Animations/LunchHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LunchHandler : MonoBehaviour {
+
+    public List<SpriteRenderer> lunchItems;
+
+    public int[] lunchAnimOrder;
+    public string[] animTriggers;
+    static int currentIndex = 0;
+
+    public void AdvanceAnimation() {
+        if (currentIndex < lunchAnimOrder.Length) {
+            SendItemAnimTrigger(lunchAnimOrder[currentIndex], animTriggers[currentIndex]);
+            currentIndex++;
+        }
+    }
+
+    void SendItemAnimTrigger(int index, string triggerName) {
+        Debug.Log("Setting Trigger " + triggerName + " for " + lunchItems[index].name);
+        lunchItems[index].GetComponent<Animator>().SetTrigger(triggerName);
+    }
+
+    void OnValidate() {
+        if (lunchAnimOrder.Length != animTriggers.Length) {
+            Array.Resize(ref animTriggers, lunchAnimOrder.Length);
+        }
+    }
+}

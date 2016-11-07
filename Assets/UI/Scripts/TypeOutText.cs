cae7a564450df673fa2366841301838af2060c99
kmoverall <kmoverall@gmail.com>
2016-11-06 23:34:43 -0500

    Added a speech bubble capable of printing out text

---
+++ Assets/UI/Scripts/TypeOutText.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypeOutText : MonoBehaviour {
+
+    Text textObject;
+    string stringToPrint = "";
+
+    public float typingSpeed = 100;
+    [HideInInspector]
+    public bool isFinished = false;
+
+    void OnEnable() {
+        PrepText();
+        stringToPrint = "TESTTESTTESTTESTTESTTEST\nTESTTESTTESTTESTTESTTESTTESTTESTTEST\nTESTESTESTESTESTEST";
+        StartTyping(stringToPrint);
+    }
+
+    public void PrepText() {
+        textObject = GetComponent<Text>();
+        textObject.text = "";
+    }
+
+    // Update is called once per frame
+    public void StartTyping(string textToType) {
+        stringToPrint = textToType;
+        StartCoroutine(TypeText());
+    }
+
+    public void Interrupt() {
+        StopCoroutine("TypeText");
+        textObject.text = stringToPrint;
+        isFinished = true;
+    }
+
+    IEnumerator TypeText() {
+        isFinished = false;
+        textObject.text = "";
+        float typeProgress = 0;
+
+        while (typeProgress < stringToPrint.Length) {
+            for (int i = Mathf.FloorToInt(typeProgress); i < Mathf.FloorToInt(typeProgress + Time.deltaTime * typingSpeed); i++) {
+                textObject.text += stringToPrint[i];
+            }
+            typeProgress += Time.deltaTime * typingSpeed;
+            yield return null;
+        }
+
+        isFinished = true;
+    }
+}

1a5538c654c64737b2393423b0e8518a77bfe0f4
kmoverall <kmoverall@gmail.com>
2016-11-11 23:36:25 -0500

    Added fun little sounds to the speech bubble!

--- Assets/UI/Scripts/TypeOutText.cs
+++ Assets/UI/Scripts/TypeOutText.cs
@@ -6,8 +6,10 @@ public class TypeOutText : MonoBehaviour {
 
     Text textObject;
     string stringToPrint = "";
+    float baseTalkSFXPitch = 0;
 
-    public float typingSpeed = 0.05f;
+    public float typingSpeed = 30;
+    public float talkSFXPitchVariance = 0.1f;
     [HideInInspector]
     public bool isFinished = false;
 
@@ -36,14 +38,30 @@ public class TypeOutText : MonoBehaviour {
         textObject.text = "";
         float typeProgress = 0;
 
+        PlayTalkSound();
+
         while (typeProgress < stringToPrint.Length) {
-            for (int i = Mathf.FloorToInt(typeProgress); i < Mathf.FloorToInt(typeProgress + Time.deltaTime * typingSpeed) && i < stringToPrint.Length; i++) {
+            for (int i = Mathf.FloorToInt(typeProgress); i < Mathf.FloorToInt(typeProgress + Time.deltaTime * typingSpeed) && i < stringToPrint.Length; i++) 
+            {
                 textObject.text += stringToPrint[i];
+                if (stringToPrint[i] == ' ' || stringToPrint[i] == '\n') 
+                {
+                    PlayTalkSound();
+                }
             }
             typeProgress += Time.deltaTime * typingSpeed;
             yield return null;
         }
 
         isFinished = true;
+    }
+
+    public void PlayTalkSound() {
+        if (baseTalkSFXPitch == 0) {
+            baseTalkSFXPitch = GetComponent<AudioSource>().pitch;
+        }
+
+        GetComponent<AudioSource>().pitch = baseTalkSFXPitch + Random.Range(-1 * talkSFXPitchVariance, talkSFXPitchVariance);
+        GetComponent<FadingAudioSource>().Fade(GetComponent<AudioSource>().clip, 1, false);
     }
 }

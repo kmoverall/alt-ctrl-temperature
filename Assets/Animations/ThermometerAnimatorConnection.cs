7e32b5ad966762e59e3e9c5829fb18588ce9753d
kmoverall <kmoverall@gmail.com>
2016-11-12 20:12:36 -0500

    Added a mid-game check in with dialogue tree

--- Assets/Animations/ThermometerAnimatorConnection.cs
+++ Assets/Animations/ThermometerAnimatorConnection.cs
@@ -6,6 +6,10 @@ public class ThermometerAnimatorConnection : MonoBehaviour {
 
     void Update() {
         GetComponent<Animator>().SetBool("HeadCheck", Thermometer.isOnHead);
+        GetComponent<Animator>().SetBool("Fever", Mathf.Round(Thermometer.temperature) >= GameManager.parameters.feverTemperature);
+        GetComponent<Animator>().SetBool("Hypothermia", Mathf.Round(Thermometer.temperature) <= GameManager.parameters.hypothermiaTemperature);
+        GetComponent<Animator>().SetBool("Healthy", Mathf.Round(Thermometer.temperature) > GameManager.parameters.hypothermiaTemperature &&
+                                                    Mathf.Round(Thermometer.temperature) < GameManager.parameters.feverTemperature);
     }
 	
 }

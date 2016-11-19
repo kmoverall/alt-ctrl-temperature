db00941d85a893fa2db536c7120c85034f518b35
kmoverall <kmoverall@gmail.com>
2016-11-18 21:34:25 -0500

    Added temperature and on head overrides to debug manager. Added keyboard shortcuts for debug manager

--- Assets/Managers/Input/Thermometer.cs
+++ Assets/Managers/Input/Thermometer.cs
@@ -8,14 +8,14 @@ public class Thermometer : Singleton<Thermometer> {
     protected Thermometer() { }
 
     float _temp = 60;
-    public static float temperature { get { return Instance._temp; } }
+    public static float temperature { get { return !DebugManager.DebugMode ? Instance._temp : DebugManager.temperature; } }
 
     List<float> _record = new List<float>();
     int _smoothing = 30;
     public static int smoothing { get { return Instance._smoothing; } set { Instance._smoothing = value; } }
     
     bool _isOnHead = false;
-    public static bool isOnHead { get { return Instance._isOnHead; } }
+    public static bool isOnHead { get { return !DebugManager.DebugMode ? Instance._isOnHead : DebugManager.isOnHead; } }
 
     public static string displayTemperature { 
         get { 
@@ -25,41 +25,37 @@ public class Thermometer : Singleton<Thermometer> {
     }
     
     void Update() {
-
-        //Get Temperature Data
-        if (SerialInputManager.serialData == null || SerialInputManager.serialData.Length == 0) {
-            return;
-        }
-
-        float t = Convert.ToInt32(SerialInputManager.serialData[0]);
-        //Convert raw reading to temperature
-        t = t / 128f;
-        t = t * 1.8f + 32;
-
-        //Add new temperature to temperature record
-        if (_record.Count == smoothing) {
-            _record.RemoveAt(0);
-        }
-        _record.Add(t);
-
-        //Take average of past several temperatures
-        _temp = 0;
-        foreach (float f in _record) {
-            _temp += f;
-        }
-        _temp /= _record.Count;
-
-        //Get Placement Data
-        if (SerialInputManager.serialData.Length == 1) {
-            return;
-        }
-
-        //Logic for head sensor is reversed
         if (!DebugManager.DebugMode) {
+            //Get Temperature Data
+            if (SerialInputManager.serialData == null || SerialInputManager.serialData.Length == 0) {
+                return;
+            }
+
+            float t = Convert.ToInt32(SerialInputManager.serialData[0]);
+            //Convert raw reading to temperature
+            t = t / 128f;
+            t = t * 1.8f + 32;
+
+            //Add new temperature to temperature record
+            if (_record.Count == smoothing) {
+                _record.RemoveAt(0);
+            }
+            _record.Add(t);
+
+            //Take average of past several temperatures
+            _temp = 0;
+            foreach (float f in _record) {
+                _temp += f;
+            }
+            _temp /= _record.Count;
+
+            //Get Placement Data
+            if (SerialInputManager.serialData.Length == 1) {
+                return;
+            }
+
+            //Logic for head sensor is reversed
             _isOnHead = SerialInputManager.serialData[1] == "0";
-        }
-        else {
-            _isOnHead = true;
         }
     }
 }
--- Assets/Utility/DebugManager.cs
+++ Assets/Utility/DebugManager.cs
@@ -8,12 +8,40 @@ public class DebugManager : Singleton<DebugManager> {
     public bool debugMode = false;
     public static bool DebugMode { get { return Instance.debugMode; } }
 
+    public bool _isOnHead = false;
+    public static bool isOnHead { get { return Instance._isOnHead; } }
+
+    public float _temperature = 90;
+    public static float temperature { get { return Instance._temperature;  } }
+
     [Range(0, 8)]
     public float debugTimeScale = 1;
     public static float TimeScale { get { return Instance.debugTimeScale; } }
 	
 	void Update () {
-        if (debugMode)
+        if (Input.GetKeyDown(KeyCode.BackQuote)) {
+            debugMode = true;
+        }
+
+        if (debugMode) {
+            if (Input.GetKeyDown(KeyCode.KeypadPlus)) {
+                _temperature++;
+            }
+            else if (Input.GetKeyDown(KeyCode.KeypadMinus)) {
+                _temperature--;
+            }
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                _isOnHead = !_isOnHead;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                debugTimeScale *= 2;
+                debugTimeScale = Mathf.Min(8, debugTimeScale);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                debugTimeScale /= 2;
+            }
             Time.timeScale = debugTimeScale;
+        }
 	}
 }

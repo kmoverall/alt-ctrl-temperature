ed2d8adad8ab69ecb7e1db9bc2e38460d43ba2df
kmoverall <kmoverall@gmail.com>
2016-11-20 15:36:53 -0500

    Indicator for whether thermometer is on head is now only visible in debug mode

--- Assets/UI/Scripts/UIPlacementIndicator.cs
+++ Assets/UI/Scripts/UIPlacementIndicator.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class UIPlacementIndicator : MonoBehaviour {
 	void Update () {
+        GetComponent<Image>().enabled = DebugManager.DebugMode;
         GetComponent<Image>().color = Thermometer.isOnHead ? Color.green : Color.red;
 	}
 }

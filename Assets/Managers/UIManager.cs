07f799b53d506f752a3ac5a1606cff52bce58755
kmoverall <kmoverall@gmail.com>
2016-11-27 17:06:16 -0500

    Added variable text depending on end conditions

--- Assets/Managers/UIManager.cs
+++ Assets/Managers/UIManager.cs
@@ -11,8 +11,21 @@ public class UIManager : Singleton<UIManager>
     protected UIManager() { }
 
     public GameObject mainMenu;
+    public Text gameOverText;
+    public Text gameOverSubText;
+
+    public string loseString;
+    public Color loseColor;
+    public string winString;
+    public Color winColor;
 
     public static void FireMenuAnimation(string trigger) {
         Instance.mainMenu.GetComponent<Animator>().SetTrigger(trigger);
+    }
+
+    public static void SetFinalUIText(bool success, string subText) {
+        Instance.gameOverText.text = success ? Instance.winString : Instance.loseString;
+        Instance.gameOverText.color = success ? Instance.winColor : Instance.loseColor;
+        Instance.gameOverSubText.text = subText;
     }
 }
